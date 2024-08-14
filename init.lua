25b0dfbc2477de261d8203c032e936c1d1e6a252
Daveanand Mannie <daveanandmannie@gmail.com>
2024-08-14 12:41:07 -0400

    updated config

--- init.lua
+++ init.lua
@@ -4,16 +4,10 @@ require("nvim-treesitter.install").compilers = { "zig" }
 
 -- colourscheme
 vim.cmd("colorscheme wildcharm")
-vim.g.python3_host_prog = 'C:/Users/support/Desktop/workspace/vimenv/Scripts/python.exe'
 
 -- styling cuz im dumb and dont really know what im doing with this conifg
-vim.o.tabstop = 4
-vim.o.expandtab = true
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-
 -- increase the time before whichkey pops up
-vim.o.timeoutlen = 1000
+vim.o.timeoutlen = 1400
 
 -- show hidden files in neo tree
 require('neo-tree').setup{
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options hereby
 vim.g.autoformat = false
+
+vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_ruff = "ruff"
+
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
