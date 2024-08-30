09fdbf5dbef167ac980bc8447a133c13020db2bc
Daveanand Mannie <daveanandmannie@gmail.com>
2024-08-30 14:39:06 -0400

    made it so the cli is powershell

--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
+--  powershell so my shit compiles with zig when i install
+vim.o.shell = "powershell"
+vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+vim.o.shellquote = ""
+vim.o.shellxquote = ""
 -- colourscheme
 vim.cmd("colorscheme wildcharm")
 
