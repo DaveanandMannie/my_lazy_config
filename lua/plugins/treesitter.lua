5aef885a3ade991dedb4c0ca386c817d5288d100
Daveanand Mannie <daveanandmannie@gmail.com>
2024-09-02 20:41:26 -0400

    removed dup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = function()
-      require("nvim-treesitter.install").compilers = { "zig" }
     end,
     opts = {
       highlight = { enable = true },
