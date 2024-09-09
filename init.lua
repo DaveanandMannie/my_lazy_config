d797105520f448fd5a89cfa3535724a5ef017cb3
Daveanand Mannie <daveanandmannie@gmail.com>
2024-09-09 15:10:39 -0400

    revert to powershell 5

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 --  powershell so my shit compiles with zig when i install
-vim.o.shell = "C:/Program Files/PowerShell/7/pwsh.exe"
+vim.o.shell = "powershell"
+vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
 vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
 vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
 vim.o.shellquote = ""
