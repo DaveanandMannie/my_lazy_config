4042ad5e9f29bacfd198cf9efdb5ca03b40d2c10
Daveanand Mannie <dave@printgeek.ca>
2025-03-28 03:37:47 -0400

    [REMOVE] removed redunat lint key

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -5,8 +5,5 @@ return {
     lint.linters_by_ft = {
       python = { "flake8" },
     }
-    vim.keymap.set("n", "<leader>l", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
   end,
 }
