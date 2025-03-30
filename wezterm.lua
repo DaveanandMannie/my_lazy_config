02531b97745975057fd4b723f6cc2f61bd00c7bd
Daveanand Mannie <dave@printgeek.ca>
2025-03-30 18:37:51 -0400

    [FIX] bad pwsh args

--- wezterm.lua
+++ wezterm.lua
@@ -2,9 +2,9 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 -- workspace dir default
-config.default_cwd = "C:/Users/support/workspace"
+config.default_cwd = os.getenv("HOMEPATH") .. "/workspace"
 -- default term prog can be changed with cli args
-config.default_prog = { "C:/Program Files/PowerShell/7/pwsh.exe", "-nologo" }
+config.default_prog = { "pwsh" }
 
 --styles
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
