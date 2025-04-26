bb8dbfaeb4396084e9503788f3d5c9cadf8055b3
Daveanand Mannie <dave@printgeek.ca>
2025-04-26 18:26:38 -0400

    [ADDED] biome general config and portal alias

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -9,4 +9,9 @@ alias fd='fdfind'
 alias sudo-shell='sudo -E zsh'
 
 # [Development]
+# [Odoo]
 alias erp-dev='cd ~/workspace/pg/erp/ && source venvs/odoo17/bin/activate'
+alias erp-dev-server='erp-dev && cd odoo && python3 odoo-bin -c ../.odoorc --dev all'
+
+# [portal]
+alias erp-portal-server='erp-dev && cd odoo && python3 odoo-bin -c ../.portalrc --dev all'
