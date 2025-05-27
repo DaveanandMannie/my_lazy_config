8af61eefc33357cceed5985e7d23dcf733ce33bd
Daveanand Mannie <dave@printgeek.ca>
2025-05-27 13:29:16 -0400

    [IMP] Improved aliases for starting and stopping server and getting to repo roots

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -14,6 +14,6 @@ alias erp-dev='cd ~/workspace/pg/erp/'
 alias erp-dev-server='erp-dev && source venvs/odoo17/bin/activate && cd odoo && python3 odoo-bin -c ../.odoorc --dev all'
 
 # [portal]
-alias erp-portal-server='erp-dev && cd odoo && python3 odoo-bin -c ../.portalrc --dev all'
+alias erp-portal-server='erp-dev && source venvs/odoo17/bin/activate && cd odoo && python3 odoo-bin -c ../.portalrc --dev all'
 alias portal-dev='cd ~/workspace/pg/portal/'
 
