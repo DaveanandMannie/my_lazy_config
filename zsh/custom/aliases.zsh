d11d3003ad133713278a3391f72dbb328fd6b719
Daveanand Mannie <dave@printgeek.ca>
2025-10-15 20:54:53 -0400

    [CHORE] qol aliases and wezterm size

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -9,8 +9,10 @@ alias fd='fdfind'
 alias sudo-shell='sudo -E zsh'
 
 # [Development]
+alias wezterm='/mnt/c/Users/DaveanandMannie/scoop/apps/wezterm/current/wezterm.exe'
 # [Odoo]
-alias erp-dev='cd ~/workspace/pg/erp_legacy/'
+alias erp-dev='cd ~/workspace/pg/erp/'
+alias erp-dev-server='erp-dev && source venvs/odoo18/bin/activate && cd odoo && python3 odoo-bin -c ../odoo.conf --dev all'
 alias erp-dev-test='cd ~/workspace/pg/erp && source venvs/odoo18/bin/activate && cd odoo && python3 odoo-bin -c ../test.conf -i accounting_td_edi,anb_img_zip_importation,auto_raw_procure,client_sku_mapping,contact_anonymization,invoice_line_grouper,pgk_dashboard,pgk_purchase,purchase_order_cost_approval,pricing_breakdown,stock_picking_batch_simple,roq_integration && dropdb --if-exists testdb'
 # [portal]
 alias erp-portal-server='erp-dev && source venvs/odoo17/bin/activate && cd odoo && python3 odoo-bin -c ../.portalrc --dev all'
